07c0547b9dc3b3a3fcd317ff3a4866d6ea958a7b
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-26 16:07:48 +0000

    SHA256

--- hello.vb
+++ hello.vb
@@ -1,3 +1,20 @@
-Sub HelloWorld()
-    MsgBox "Hello World"
-End Sub
+Imports System.Security.Cryptography
+Imports System.Text
+
+Module Module1
+
+    Sub Main()
+        Dim message As String = "สวัสดี ชาวโลก"
+        Dim hashValue As Byte()
+
+        Using hash As HashAlgorithm = SHA256.Create()
+            hashValue = hash.ComputeHash(Encoding.UTF8.GetBytes(message))
+        End Using
+
+        Console.WriteLine("ข้อความรหัส: ")
+        For Each b As Byte In hashValue
+            Console.Write($"{b:x2}")
+        Next
+    End Sub
+
+End Module
